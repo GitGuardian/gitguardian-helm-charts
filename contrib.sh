30474b97e215ce614e7a3926043dadd52cad4ddf
Frederic Spiers <frederic.spiers@gitguardian.com>
2025-10-09 14:50:51 +0200

    chore(contrib): update contrib script

--- contrib.sh
+++ contrib.sh
@@ -4,9 +4,9 @@ ACTION=$1
 BRANCH_NAME=$2
 
 if [ -z "$BRANCH_NAME" ]; then
-    echo "Usage: $0 <branch-name> [start|finish|update]"
-    echo "  start:  Start new contribution"
-    echo "  finish: Restore private workflows"
+    echo "Usage: $0 <branch-name> [start|finish]"
+    echo "  start: Start new contribution"
+    echo "  finish: Restore upstream diff"
     exit 1
 fi
 
@@ -14,16 +14,23 @@ case $ACTION in
     "start")
         echo "🔄 Synchronisation with upstream..."
         git checkout main
+        git fetch origin
 
         git fetch upstream
-        git diff upstream/main -- .github/workflows/ > private-workflows.patch
+        git diff upstream/main > diff-upstream.patch
         
-        echo "🗑️ Deleting private workflows before contrib..."
-        git apply -R private-workflows.patch
-        echo "✅ Private workflows deleted"
+        echo "🗑️ Deleting upstream diff before contrib..."
+        git apply -R diff-upstream.patch
+        echo "✅ Upstream diff deleted"
         
         echo "🌿 Creating branch $BRANCH_NAME..."
         git checkout -b "$BRANCH_NAME"
+
+        echo "🌿 Prepare contrib by deleting diff"
+        git add -A
+        git reset HEAD diff-upstream.patch
+        git commit -m "refactor(contrib): prepare contribution"
+        git push origin "$BRANCH_NAME"
         
         echo "✅ Ready for contrib !"
         ;;
@@ -32,14 +39,14 @@ case $ACTION in
         echo "🔄 Return to main..."
         git checkout main
         
-        echo "🔧 Restore private workflows..."
-        git apply private-workflows.patch
+        echo "🔧 Restore upstream diff workflows..."
+        git apply diff-upstream.patch
                 
         echo "✅ Ready !"
         ;;
          
     *)
-        echo "Action inconnue: $ACTION"
-        echo "Utilisez: start or finish"
+        echo "Unkown action: $ACTION"
+        echo "Use start or finish"
         ;;
 esac
